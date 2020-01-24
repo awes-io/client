e0684ba848dc3e6a42d522b240ddf098f9cfbeff
illjah <illjah@bk.ru>
2020-01-24 16:50:45 +0200

    docs: vuese doc comments added to aw-multi-block-builder

--- packages/ui/components/AwMultiBlockBuilder.vue
+++ packages/ui/components/AwMultiBlockBuilder.vue
@@ -16,6 +16,7 @@
                     collectionCount
                 }"
             >
+                <!-- `<h5>{title} #{iteration} <button>Remove {title}</button></h5>` -->
                 <h5 class="flex items-center draggable">
                     <span>
                         {{
@@ -29,7 +30,7 @@
                         icon="close"
                         theme="icon"
                         :text="$t('AwMultiBlockBuilder.remove')"
-                        @click="collection.remove(model)"
+                        @click="remove(model)"
                     />
                 </h5>
             </slot>
@@ -40,11 +41,9 @@
 
         <!-- add button -->
         <div class="mt-8" v-if="collectionCount < max">
-            <slot name="add" v-bind="{ collection, title }">
-                <AwLink
-                    class="text-sm font-bold uppercase"
-                    @click.prevent="collection.add()"
-                >
+            <slot name="add" v-bind="{ collection, title, add }">
+                <!-- `<button>+ Add {title}</button>` -->
+                <AwLink class="text-sm font-bold uppercase" @click="add">
                     {{ $t('AwMultiBlockBuilder.add', { title }) }}
                 </AwLink>
             </slot>
@@ -52,6 +51,7 @@
 
         <!-- save button -->
         <slot name="save" v-bind="{ collectionChanged, collection }">
+            <!-- `<button>Save</button>` -->
             <div class="mt-8">
                 <AwButton
                     :disabled="!collectionChanged || collectionRequest"
@@ -68,10 +68,28 @@
 import { equals, prop, isNil, clone } from 'rambdax'
 import { validateBySchema } from '../assets/js/component'
 
+/**
+ * Serves for VueMC collection visualization. Pass the collection to `collection` prop, and form fields that you need to default slot.
+ *
+ * ```
+ * <AwMultiBlockBuilder title="Manager" :collection="managers">
+ *    <template #default="{ model }">
+ *      <AwInput
+ *         v-model="model.first_name"
+ *         :error="model.errors.first_name"
+ *         label="First name"
+ *       />
+ *    </template>
+ *  </AwMultiBlockBuilder>
+ *  ```
+ */
 export default {
     name: 'AwMultiBlockBuilder',
 
     props: {
+        /**
+         * VueMC collection object
+         */
         collection: {
             type: Object,
             required: true,
@@ -84,11 +102,17 @@ export default {
             })
         },
 
+        /**
+         * Title, added to block heading and buttons
+         */
         title: {
             type: String,
             default: ''
         },
 
+        /**
+         * Max number of items, the add button hides if value is reached
+         */
         max: {
             type: Number,
             default: Infinity
@@ -124,15 +148,12 @@ export default {
     },
 
     mounted() {
-        const fetchListener = this.getEventListener('fetch')
-        const saveListener = this.getEventListener('save')
-
-        this.collection.on('fetch', fetchListener)
-        this.collection.on('save', saveListener)
+        this.collection.on('fetch', this.fetchListener)
+        this.collection.on('save', this.saveListener)
 
         this.$once('hook:beforeDestroy', () => {
-            this._removeCollectionListener('fetch', fetchListener)
-            this._removeCollectionListener('save', saveListener)
+            this._removeCollectionListener('fetch', this.fetchListener)
+            this._removeCollectionListener('save', this.saveListener)
         })
 
         // initial fetch
@@ -140,18 +161,58 @@ export default {
     },
 
     methods: {
-        getEventListener(name) {
-            return $event => {
-                if (isNil($event.error)) {
-                    this.updateSnapshot()
-                }
-
-                this.$emit(`after-${name}`, this.collection, $event)
-            }
+        /**
+         * @vuese
+         * Add new model and emit `add:model` event
+         */
+        add() {
+            this.collection.add()
+            /**
+             * Fire when model is added
+             * @arg `collection`, passed to props
+             */
+            this.$emit('add:model', this.collection)
         },
 
-        updateSnapshot() {
-            this.modelsSnapshot = clone(this.currentModels)
+        /**
+         * @vuese
+         * Remove model and emit `remove:model` event
+         * @arg The model object, required
+         */
+        remove(model) {
+            if (!model) return
+            this.collection.remove(model)
+            /**
+             * Fire when model is removed
+             * @arg `collection`, passed to props
+             */
+            this.$emit('remove:model', this.collection)
+        },
+
+        fetchListener($event) {
+            this._updateSnapshot($event)
+            /**
+             * Fire on collection fetch
+             * @arg 1.`collection`, passed to props
+             * @arg 2. VueMC event object
+             */
+            this.$emit('fetch:collection', this.collection, $event)
+        },
+
+        saveListener($event) {
+            this._updateSnapshot($event)
+            /**
+             * Fire on collection save
+             * @arg 1.`collection`, passed to props
+             * @arg 2. VueMC event object
+             */
+            this.$emit('save:collection', this.collection, $event)
+        },
+
+        _updateSnapshot($event = {}) {
+            if (isNil($event.error)) {
+                this.modelsSnapshot = clone(this.currentModels)
+            }
         },
 
         _removeCollectionListener(event, listener) {
