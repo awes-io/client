099e9a587f0b055e568e8b314c7f87ff08d8349c
illjah <illjah@bk.ru>
2020-01-31 20:02:33 +0200

    fix(vue-mc): build transpilation added for original vue-mc compatibility

---
+++ packages/vue-mc/rollup.config.js
@@ -0,0 +1,24 @@
+import babel from 'rollup-plugin-babel'
+
+const baseConfig = {
+    input: 'index.js',
+    plugins: [babel()],
+    external: ['vue', 'vue-mc', 'rambdax']
+}
+
+export default [
+    {
+        ...baseConfig,
+        output: {
+            file: 'dist/index.js',
+            format: 'cjs'
+        }
+    },
+    {
+        ...baseConfig,
+        output: {
+            file: 'dist/index.esm.js',
+            format: 'esm'
+        }
+    }
+]
