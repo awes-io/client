784a539b53e33afdff3800c21ff3d47f5a740d11
Vitaliy Baychura <vitaliy.baychura@gmail.com>
2020-02-06 11:10:16 +0200

    docs(ui): add AwAccordionFold example

--- examples/basic-ui/pages/aw-accordion-fold.vue
+++ examples/basic-ui/pages/aw-accordion-fold.vue
@@ -1,8 +1,15 @@
 <template>
     <AwPage :title="headline">
-        <AwGrid :col="{ md: 4 }">
-            <AwAccordionFold />
-        </AwGrid>
+        <AwButton @click="show = !show" size="sm">Toggle</AwButton>
+
+        <AwAccordionFold :show="show" tag="div">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            provident nemo dolores dicta dolor quia aspernatur dignissimos, in,
+            fugit animi, cumque mollitia nesciunt vitae! Et, aliquid maiores?
+            Aspernatur, error odit?
+        </AwAccordionFold>
+
+        <AwCodeSnippet v-text="code" class="mt-3" />
     </AwPage>
 </template>
 
@@ -11,9 +18,24 @@ export default {
     data() {
         return {
             title: 'AwAccordionFold',
-            headline: this._getTitle('AwAccordionFold')
+            headline: this._getTitle('AwAccordionFold'),
+            show: false
         }
     },
+
+    computed: {
+        code() {
+            const code = [
+                '<AwButton @click="show = !show" size="sm">Toggle</AwButton>',
+                '',
+                '<AwAccordionFold :show="show" tag="div">',
+                '   <!-- content here -->',
+                '</AwAccordionFold>'
+            ]
+            return code.join('\n')
+        }
+    },
+
     head() {
         return {
             title: this._getMetaTitle(this.title),
