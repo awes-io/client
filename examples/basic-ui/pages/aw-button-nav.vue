36d0727b5aafc041ba13190b52a9f6236e3ae67d
Vitaliy Baychura <vitaliy.baychura@gmail.com>
2020-02-06 13:26:50 +0200

    docs: add AwButtonNav example

--- examples/basic-ui/pages/aw-button-nav.vue
+++ examples/basic-ui/pages/aw-button-nav.vue
@@ -1,6 +1,9 @@
 <template>
     <AwPage :title="headline">
         <AwButtonNav :items="filters" />
+
+        <AwCodeSnippet v-text="codeMarkup" class="mt-5" />
+        <AwCodeSnippet v-text="codeJs" class="mt-1" language="js" />
     </AwPage>
 </template>
 
@@ -19,6 +22,37 @@ export default {
             ]
         }
     },
+
+    computed: {
+        codeMarkup() {
+            const arr = [
+                '<template>',
+                '   <AwButtonNav :items="filters" />',
+                '</template>'
+            ]
+            return arr.join('\n')
+        },
+
+        codeJs() {
+            const arr = [
+                '<script>',
+                'export default {',
+                '   data() {',
+                '       return {',
+                '           filters: [',
+                "               { text: 'Order default', href: { query: { orderBy: null } } },",
+                "               { text: 'Order by name', href: { query: { orderBy: 'name' } } },",
+                "               { text: 'Order by id', href: { query: { orderBy: 'id' } } }",
+                '           ]',
+                '       }',
+                '   }',
+                '}',
+                '</' + 'script>'
+            ]
+            return arr.join('\n')
+        }
+    },
+
     head() {
         return {
             title: this._getMetaTitle(this.title),
