79dd2e505d09d919745e3bda478c560162706966
Vitaliy Baychura <vitaliy.baychura@gmail.com>
2020-02-12 21:52:04 +0200

    docs: add AwChipSelect docs

--- examples/basic-ui/pages/aw-chip-select.vue
+++ examples/basic-ui/pages/aw-chip-select.vue
@@ -14,6 +14,8 @@
                 </template>
             </AwTableCol>
         </AwTable>
+
+        <AwCodeSnippet v-text="code" class="mt-3" :size="50" />
     </AwPage>
 </template>
 
@@ -73,6 +75,23 @@ export default {
             title: 'AwChipSelect',
             headline: this._getTitle('AwChipSelect'),
             appointments
+        }
+    },
+
+    computed: {
+        code() {
+            const arr = [
+                '<AwChipSelect',
+                '   :options="[',
+                "       {id: 1, text: 'New', color: 'info', icon: 'plus-solid'}",
+                "       {id: 2, text: 'Pending', color: 'warning', icon: 'triangle-solid-r'}",
+                "       {id: 3, text: 'Active', color: 'success', icon: 'check-solid'}",
+                "       {id: 4, text: 'Removed', color: 'error', icon: 'close-solid'}",
+                '   ]"',
+                '   :loading="true"',
+                '/>'
+            ]
+            return arr.join('\n')
         }
     },
 
