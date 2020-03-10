73311cb5e95795bea13bfa08c7928fe309772f57
begimov <begimov@gmail.com>
2020-03-10 13:53:41 +0300

    fix: useless tags removed

--- packages/nuxt-admin/src/pages/role/index.vue
+++ packages/nuxt-admin/src/pages/role/index.vue
@@ -1,36 +1,34 @@
 <template>
-    <div>
-        <AwGrid :col="{ lg: 2 }">
-            <div>
-                <p class="h6">{{ $t('AwesIoNuxtAdmin.role_name') }}</p>
-                <AwInput
-                    v-model="model.name"
-                    name="name"
-                    :error="model.errors.name"
-                />
-                <AwSwitcher 
-                    class="mt-5"
-                    v-model="model.default_new"
-                    :label="$t('AwesIoNuxtAdmin.default_role')"
-                />
-                <p class="h6">{{ $t('AwesIoNuxtAdmin.priority') }}</p>
-                <AwInput
-                    v-model="model.priority"
-                    name="priority"
-                    :error="model.errors.priority"
-                />
-                <p class="h6">{{ $t('AwesIoNuxtAdmin.description') }}</p>
-                <AwTextarea
-                    v-model="model.description"
-                    name="description"
-                    :error="model.errors.description"
-                />
-                <AwButton class="mt-5" @click="updateRole" :loading="model.saving">
-                    {{ $t('AwesIoNuxtAdmin.update') }}
-                </AwButton>
-            </div>
-        </AwGrid>
-    </div>
+    <AwGrid :col="{ lg: 2 }">
+        <div>
+            <p class="h6">{{ $t('AwesIoNuxtAdmin.role_name') }}</p>
+            <AwInput
+                v-model="model.name"
+                name="name"
+                :error="model.errors.name"
+            />
+            <AwSwitcher 
+                class="mt-5"
+                v-model="model.default_new"
+                :label="$t('AwesIoNuxtAdmin.default_role')"
+            />
+            <p class="h6">{{ $t('AwesIoNuxtAdmin.priority') }}</p>
+            <AwInput
+                v-model="model.priority"
+                name="priority"
+                :error="model.errors.priority"
+            />
+            <p class="h6">{{ $t('AwesIoNuxtAdmin.description') }}</p>
+            <AwTextarea
+                v-model="model.description"
+                name="description"
+                :error="model.errors.description"
+            />
+            <AwButton class="mt-5" @click="updateRole" :loading="model.saving">
+                {{ $t('AwesIoNuxtAdmin.update') }}
+            </AwButton>
+        </div>
+    </AwGrid>  
 </template>
 
 <script>
--- packages/nuxt-admin/src/pages/user/index.vue
+++ packages/nuxt-admin/src/pages/user/index.vue
@@ -1,87 +1,85 @@
 <template>
-    <div>
-        <AwGrid :col="{ lg: 2 }">
-            <div>
-                <p class="h6">{{ $t('AwesIoNuxtAdmin.first_name') }}</p>
-                <AwInput
-                    v-model="model.profile.first_name"
-                    name="first_name"
-                    :error="model.errors['profile.first_name']"
-                />
-            </div>
-            <div>
-                <p class="h6">{{ $t('AwesIoNuxtAdmin.last_name') }}</p>
-                <AwInput
-                    v-model="model.profile.last_name"
-                    name="last_name"
-                    :error="model.errors['profile.last_name']"
-                />
-            </div>
-            <div :span="{ lg: 2 }">
-                <AwRadio
-                    class="inline-block"
-                    v-model="model.profile.sex"
-                    :label="$t('AwesIoNuxtAdmin.male')"
-                    value="male"
-                    :error="model.errors['profile.sex']"
-                />
-                <AwRadio
-                    class="inline-block ml-3"
-                    v-model="model.profile.sex"
-                    :label="$t('AwesIoNuxtAdmin.female')"
-                    value="female"
-                />
-            </div>
-            <div>
-                <p class="h6">{{ $t('AwesIoNuxtAdmin.email') }}</p>
-                <AwInput
-                    v-model="model.email"
-                    name="email"
-                    :error="model.errors.email"
-                />
-            </div>
-            <div>
-                <p class="h6">{{ $t('AwesIoNuxtAdmin.phone') }}</p>
-                <AwInput
-                    v-model="model.profile.phone"
-                    name="phone"
-                    :error="model.errors['profile.phone']"
-                />
-            </div>
-            <div>
-                <p class="h6">{{ $t('AwesIoNuxtAdmin.position') }}</p>
-                <AwInput
-                    v-model="model.profile.position"
-                    name="position"
-                    :error="model.errors['profile.position']"
-                />
-            </div>
-            <div>
-                <p class="h6">{{ $t('AwesIoNuxtAdmin.role') }}</p>
-                <AwSelect
-                    v-model="model.role.id"
-                    :label="$t('AwesIoNuxtAdmin.role')"
-                    :options="(text) => `/api/admin/roles?name=${text}&limit=100`"
-                    option-label="name"
-                    option-value="id"
-                    :error="model.errors['role.id']"
-                />
-            </div>
-            <div :span="{ lg: 2 }">
-                <p class="h6">{{ $t('AwesIoNuxtAdmin.description') }}</p>
-                <AwTextarea
-                    v-model="model.profile.description"
-                    name="description"
-                    :error="model.errors['profile.description']"
-                />
-            </div>
-            <div :span="{ lg: 2 }">
-                <AwButton class="mt-5" @click="updateUser" :loading="model.saving">
-                    {{ $t('AwesIoNuxtAdmin.update') }}
-                </AwButton>
-            </div>
-        </AwGrid>
-    </div>
+    <AwGrid :col="{ lg: 2 }">
+        <div>
+            <p class="h6">{{ $t('AwesIoNuxtAdmin.first_name') }}</p>
+            <AwInput
+                v-model="model.profile.first_name"
+                name="first_name"
+                :error="model.errors['profile.first_name']"
+            />
+        </div>
+        <div>
+            <p class="h6">{{ $t('AwesIoNuxtAdmin.last_name') }}</p>
+            <AwInput
+                v-model="model.profile.last_name"
+                name="last_name"
+                :error="model.errors['profile.last_name']"
+            />
+        </div>
+        <div :span="{ lg: 2 }">
+            <AwRadio
+                class="inline-block"
+                v-model="model.profile.sex"
+                :label="$t('AwesIoNuxtAdmin.male')"
+                value="male"
+                :error="model.errors['profile.sex']"
+            />
+            <AwRadio
+                class="inline-block ml-3"
+                v-model="model.profile.sex"
+                :label="$t('AwesIoNuxtAdmin.female')"
+                value="female"
+            />
+        </div>
+        <div>
+            <p class="h6">{{ $t('AwesIoNuxtAdmin.email') }}</p>
+            <AwInput
+                v-model="model.email"
+                name="email"
+                :error="model.errors.email"
+            />
+        </div>
+        <div>
+            <p class="h6">{{ $t('AwesIoNuxtAdmin.phone') }}</p>
+            <AwInput
+                v-model="model.profile.phone"
+                name="phone"
+                :error="model.errors['profile.phone']"
+            />
+        </div>
+        <div>
+            <p class="h6">{{ $t('AwesIoNuxtAdmin.position') }}</p>
+            <AwInput
+                v-model="model.profile.position"
+                name="position"
+                :error="model.errors['profile.position']"
+            />
+        </div>
+        <div>
+            <p class="h6">{{ $t('AwesIoNuxtAdmin.role') }}</p>
+            <AwSelect
+                v-model="model.role.id"
+                :label="$t('AwesIoNuxtAdmin.role')"
+                :options="(text) => `/api/admin/roles?name=${text}&limit=100`"
+                option-label="name"
+                option-value="id"
+                :error="model.errors['role.id']"
+            />
+        </div>
+        <div :span="{ lg: 2 }">
+            <p class="h6">{{ $t('AwesIoNuxtAdmin.description') }}</p>
+            <AwTextarea
+                v-model="model.profile.description"
+                name="description"
+                :error="model.errors['profile.description']"
+            />
+        </div>
+        <div :span="{ lg: 2 }">
+            <AwButton class="mt-5" @click="updateUser" :loading="model.saving">
+                {{ $t('AwesIoNuxtAdmin.update') }}
+            </AwButton>
+        </div>
+    </AwGrid>
 </template>
 
 <script>
