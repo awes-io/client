59124087ff92f25387b5f12875aa465961414558
begimov <begimov@gmail.com>
2020-03-10 13:22:03 +0300

    feat: updatin role permissions

--- packages/nuxt-admin/src/pages/role/permissions.vue
+++ packages/nuxt-admin/src/pages/role/permissions.vue
@@ -3,16 +3,15 @@
         <AwTableBuilder :collection="allPermissions">
             <AwTableCol>
                 <template #default="{ cell }">
-                    <AwCheckbox :value="cell.id.toString()" v-model="model.permissions" :error="model.errors.permissions" />
+                    <AwCheckbox :value="cell.id.toString()" v-model="selectedPermissions" :error="model.errors.permissions" />
                 </template>
             </AwTableCol>
             <AwTableCol field="description" :title="$t('AwesIoNuxtAdmin.description')" />
             <AwTableCol field="name" :title="$t('AwesIoNuxtAdmin.name')" />
         </AwTableBuilder>
-        <AwButton class="mt-5" @click="updateRolePermissions" :loading="model.saving">
+        <AwButton class="mt-5" @click="updateRolePermissions" :loading="permissions.saving">
             {{ $t('AwesIoNuxtAdmin.update') }}
         </AwButton>
-        <div>{{permissions.models}}</div>
     </div>
 </template>
 
@@ -36,8 +35,29 @@ export default {
     },
 
     methods: {
-        updateRolePermissions() {
-            this.permissions.save()
+        async updateRolePermissions() {
+            try {
+                await this.permissions.save()
+                this.$router.push({
+                    path: `/admin/roles`
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },
+
+    computed: {
+        selectedPermissions: {
+            get() {
+                return this.permissions.map('id').map(String)
+            },
+            set(permissions) {
+                const selected = this.allPermissions.filter(
+                    ({ id }) => permissions.includes(String(id))
+                )
+                this.permissions.replace(selected.models)
+            }
         }
     }
 }
