4d0e48d64c75a06aa01cb8ba3ac229847021a6a1
begimov <begimov@gmail.com>
2020-03-31 16:20:32 +0300

    feat: menu plugin added in admin module

---
+++ packages/nuxt-admin/src/plugins/menu.js
@@ -0,0 +1,31 @@
+export default function({ store, app }) {
+    store.commit('awesIo/SET_MENU_ITEM', [
+        {
+            key: 'admin',
+            order: 1,
+            props: {
+                text: app.i18n.t('AwesIoNuxtAdmin.admin'),
+                icon: 'user',
+                expanded: true
+            },
+            children: [
+                {
+                    key: 'admin_users',
+                    order: 2,
+                    props: {
+                        text: app.i18n.t('AwesIoNuxtAdmin.employees'),
+                        href: app.localePath({ path: '/admin/users' })
+                    }
+                },
+                {
+                    key: 'admin_roles',
+                    order: 3,
+                    props: {
+                        text: app.i18n.t('AwesIoNuxtAdmin.roles'),
+                        href: app.localePath({ path: '/admin/roles' })
+                    }
+                }
+            ]
+        },
+    ])
+}
