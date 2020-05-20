5e74c9487505fff1131a988a01c1a7c3eb74f898
Yevhen Lisovenko <yevhen.lisovenko@gmail.com>
2020-05-20 17:02:23 +0200

    fix: migrate text in for

--- packages/ui/components/AwUserMenuContent.vue
+++ packages/ui/components/AwUserMenuContent.vue
@@ -39,7 +39,8 @@
                     v-for="{
                         component,
                         key,
-                        props: { text, ...props },
+                        text,
+                        props,
                         listeners
                     } in navbarMenu"
                     :key="key"
