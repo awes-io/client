aaf7d2c89afe53343b5f4fe618a39988f57c1220
Yevhen Lisovenko <yevhen.lisovenko@gmail.com>
2020-06-19 19:35:43 +0200

    feat: removed navigation by default

--- packages/nuxt-admin/src/plugins/menu.js
+++ packages/nuxt-admin/src/plugins/menu.js
@@ -1,31 +1,35 @@
-export default function({ store, app }) {
-    store.commit('awesIo/SET_MENU_ITEM', [
-        {
-            key: 'admin',
-            order: 99,
-            props: {
-                text: () => app.i18n.t('AwesIoNuxtAdmin.admin'),
-                icon: 'user',
-                expanded: true
-            },
-            children: [
-                {
-                    key: 'admin_users',
-                    order: 2,
-                    props: {
-                        text: () => app.i18n.t('AwesIoNuxtAdmin.employees'),
-                        href: () => app.localePath({ path: '/admin/users' })
-                    }
-                },
-                {
-                    key: 'admin_roles',
-                    order: 3,
-                    props: {
-                        text: () => app.i18n.t('AwesIoNuxtAdmin.roles'),
-                        href: () => app.localePath({ path: '/admin/roles' })
-                    }
-                }
-            ]
-        }
-    ])
-}
+export default function() {}
+
+// Example: simple way how to add to navigation
+
+// export default function({ store, app }) {
+//     store.commit('awesIo/SET_MENU_ITEM', [
+//         {
+//             key: 'admin',
+//             order: 99,
+//             props: {
+//                 text: () => app.i18n.t('AwesIoNuxtAdmin.admin'),
+//                 icon: 'user',
+//                 expanded: true
+//             },
+//             children: [
+//                 {
+//                     key: 'admin_users',
+//                     order: 2,
+//                     props: {
+//                         text: () => app.i18n.t('AwesIoNuxtAdmin.employees'),
+//                         href: () => app.localePath({ path: '/admin/users' })
+//                     }
+//                 },
+//                 {
+//                     key: 'admin_roles',
+//                     order: 3,
+//                     props: {
+//                         text: () => app.i18n.t('AwesIoNuxtAdmin.roles'),
+//                         href: () => app.localePath({ path: '/admin/roles' })
+//                     }
+//                 }
+//             ]
+//         }
+//     ])
+// }
