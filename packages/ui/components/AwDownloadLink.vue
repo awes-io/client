2bdca7d8dc2a34790cdaf6ae02ddc6abba892382
Vitaliy Baychura <vitaliy.baychura@gmail.com>
2020-12-14 17:22:21 +0200

    feat(AwDownloadLink): docs, icons

--- packages/ui/components/AwDownloadLink.vue
+++ packages/ui/components/AwDownloadLink.vue
@@ -11,27 +11,31 @@
                 :color="buttonColor"
                 :size="buttonSize"
                 :disabled="loader"
+                content-class="flex items-center"
             >
-                <span class="w-5 text-center inline-block mr-1">
+                <template #default>
+                    <span class="sr-only md:not-sr-only">
+                        {{ text }}
+                    </span>
+                </template>
+
+                <template #icon>
                     <AwIcon
                         v-if="loader && isSpinner"
                         name="loader"
-                        class="animation-rotate"
+                        class="animation-rotate mr-1"
                     />
-
+                    <!-- Slot for Icon -->
                     <slot name="icon">
                         <AwIcon
                             v-if="!loader && isIcon"
-                            :name="icon"
+                            :name="_icon"
                             width="20"
                             height="20"
-                            class="fill-current inline"
+                            class="fill-current inline mr-1"
                         />
                     </slot>
-                </span>
-                <span class="sr-only md:not-sr-only">
-                    {{ text }}
-                </span>
+                </template>
             </AwButton>
 
             <span v-if="!isButton">
@@ -41,14 +45,17 @@
                         name="loader"
                         class="animation-rotate"
                     />
-                    <AwIcon
-                        v-if="!loader && isIcon"
-                        :name="icon"
-                        :class="`text-${iconColor}`"
-                        width="20"
-                        height="20"
-                        class="fill-current inline"
-                    />
+                    <!-- Slot for Icon -->
+                    <slot name="icon">
+                        <AwIcon
+                            v-if="!loader && isIcon"
+                            :name="_icon"
+                            :class="`text-${iconColor}`"
+                            width="20"
+                            height="20"
+                            class="fill-current inline"
+                        />
+                    </slot>
                 </span>
                 <AwLink :class="{ disabled: loader }">
                     <!-- The content of the link. -->
@@ -64,15 +71,48 @@
 
 <script>
 const SUPPORTED_FORMATS = {
-    pdf: 'application/pdf',
-    xls: 'application/excel',
-    xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    doc: 'application/msword',
-    docx:
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    jpeg: 'image/jpeg',
-    jpg: 'image/jpeg',
-    png: 'image/png'
+    pdf: {
+        format: 'application/pdf',
+        icon: 'fas fa-file-pdf'
+    },
+    xls: {
+        format: 'application/excel',
+        icon: 'fas fa-file-excel'
+    },
+    xlsx: {
+        format:
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        icon: 'fas fa-file-excel'
+    },
+    doc: {
+        format: 'application/msword',
+        icon: 'fas fa-file-word'
+    },
+    docx: {
+        format:
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        icon: 'fas fa-file-word'
+    },
+    jpeg: {
+        format: 'image/jpeg',
+        icon: 'fas fa-file-image'
+    },
+    jpg: {
+        format: 'image/jpeg',
+        icon: 'fas fa-file-image'
+    },
+    png: {
+        format: 'image/png',
+        icon: 'fas fa-file-image'
+    },
+    svg: {
+        format: 'image/svg+xml',
+        icon: 'fas fa-file-image'
+    },
+    csv: {
+        format: 'text/csv',
+        icon: 'fas fa-file-csv'
+    }
 }
 
 export default {
@@ -167,13 +207,12 @@ export default {
          */
         icon: {
             type: String,
-            /* solid/cloud-download */
-            default: 'fas fa-cloud-download-alt'
+            default: null
         },
         /**
          * Type of downloaded file.
          */
-        typeFile: {
+        fileType: {
             /* @String Available: `pdf`, `xlsx` */
             type: String,
             /* pdf */
@@ -188,6 +227,12 @@ export default {
         return { loader: false }
     },
 
+    computed: {
+        _icon() {
+            return this.icon || SUPPORTED_FORMATS[this.fileType].icon
+        }
+    },
+
     methods: {
         async download() {
             this.loader = true
@@ -199,31 +244,33 @@ export default {
                 const resp = await this.$axios.get(url, {
                     responseType: 'arraybuffer',
                     headers: {
-                        Accept: SUPPORTED_FORMATS[this.typeFile]
+                        Accept: SUPPORTED_FORMATS[this.fileType].format
                     }
                 })
-                console.log(resp.headers)
+
                 // Get Filename
                 const disposition = resp.headers['content-disposition']
-                const _type = `.${this.typeFile}`
+                const _type = `.${this.fileType}`
                 let filename = disposition
                     ? disposition.split('filename=')[1].replace(/"/g, '')
                     : 'file'
 
                 const extension = filename.split('.').pop()
-                if (extension !== this.typeFile) {
+                if (extension !== this.fileType) {
                     filename += _type
                 }
-                console.log(filename)
 
+                // Get File
                 const file = resp.data
                 const blob = new Blob([file], {
-                    type: SUPPORTED_FORMATS[this.typeFile]
+                    type: SUPPORTED_FORMATS[this.fileType].format
                 })
                 const blobURL = window.URL.createObjectURL(blob)
+
                 const element = document.createElement('a')
                 element.style.display = 'none'
                 element.setAttribute('href', blobURL)
+
                 // Set Filename
                 element.setAttribute('download', filename)
                 document.body.appendChild(element)
