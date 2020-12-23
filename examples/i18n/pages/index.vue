8ce2ff1ef2a0cf4418b303da45bf1e524e9026cb
Vitaliy Baychura <vitaliy.baychura@gmail.com>
2020-12-23 13:47:33 +0200

    fix(aw-filter-month): take local dayjs

--- examples/i18n/pages/index.vue
+++ examples/i18n/pages/index.vue
@@ -13,6 +13,18 @@
         <strong>Test:</strong> {{ $t('passwords.reset') }}
         <br />
         <strong>Dayjs:</strong> {{ $dayjs(new Date()).format('LL') }}
+
+        <AwDisplayDate
+            date="12.09.1995"
+            full-parse-format="DD.MM.YYYY"
+            short-parse-format="DD.MM"
+            full-display-format="ll"
+            class="block mt-4"
+        />
+
+        <div class="mt-4">
+            <AwFilterMonth display-format="MMMM YYYY" />
+        </div>
     </div>
 </template>
 
